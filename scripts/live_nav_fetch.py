a25db81cace5d597cc19ea84907c203f9416dc79
pardhu <vsaipardhu@gmail.com>
2026-06-02 12:55:22 +0530

    Day 1: Data ingestion complete

---
+++ scripts/live_nav_fetch.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+
+schemes = {
+    "hdfc_top100": 125497,
+    "sbi_bluechip": 119551,
+    "icici_bluechip": 120503,
+    "nippon_largecap": 118632,
+    "axis_bluechip": 119092,
+    "kotak_bluechip": 120841
+}
+
+for name, code in schemes.items():
+
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        data = response.json()
+
+        nav_df = pd.DataFrame(data["data"])
+
+        filename = f"data/raw/{name}_nav.csv"
+
+        nav_df.to_csv(filename, index=False)
+
+        print(f"Saved {filename}")
+
+    else:
+        print(f"Failed: {name}")
